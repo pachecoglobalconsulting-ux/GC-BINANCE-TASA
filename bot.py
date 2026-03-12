cd62e6c0b0000edc52870a00aa931229cdc0a8a2
Antonio Pacheco <apacheco.globalconsulting@gmail.com>
2026-03-12 15:12:40 -0400

    fix gunicorn deployment

--- bot.py
+++ bot.py
@@ -9,29 +9,20 @@ VERIFY_TOKEN = "binance_token"
 def home():
     return "Bot Binance activo"
 
-
 @app.route("/webhook", methods=["GET"])
 def verify_webhook():
-
     token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
-
     if token == VERIFY_TOKEN:
         return challenge
-
     return "Verification token mismatch", 403
-
 
 @app.route("/webhook", methods=["POST"])
 def recibir_mensaje():
-
     data = request.json
     print("Mensaje recibido:", data)
-
     return "ok", 200
 
-
 if __name__ == "__main__":
-
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
